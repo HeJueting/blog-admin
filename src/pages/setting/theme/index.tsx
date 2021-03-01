15b9896fc18529839116240e0bb8fefc38fd1bfc
hejueting <279975246@qq.com>
2021-03-02 00:35:49 +0800

    博客主题配置代码编写（未完成）

--- src/pages/setting/theme/index.tsx
+++ src/pages/setting/theme/index.tsx
@@ -4,8 +4,7 @@ import lodash from '../../../utils/lodash';
 import CONFIG from '../../../config';
 import settingAxios from '../../../api/setting';
 
-import { Button, Modal } from 'antd';
-import Upload from '../../../components/upload';
+import { Button, Modal, Input, message } from 'antd';
 import Icon from '../../../components/icon';
 
 // 接口：props
@@ -20,7 +19,14 @@ interface ICarsouelImageProps {
 }
 
 const Theme: React.FC<ICarsouelImageProps> = ({ data }) => {
+    // 博客配置信息
     const [themeConfig, setThemeConfig] = useState<IObj>({});
+    // 弹窗
+    const [name, setName] = useState<string>('');
+    const [obj, setObj] = useState<string>('');
+    const [editTheme, setEditTheme] = useState<string>('');
+    const [visible, setVisible] = useState<boolean>(false);
+    const [operation, setOperation] = useState<string>('create');
 
     // 初始化页面数据
     const initData = () => {
@@ -33,7 +39,7 @@ const Theme: React.FC<ICarsouelImageProps> = ({ data }) => {
     }, [data]);
 
     // 点击保存（博客首页）
-    const save = async () => {
+    const saveOnClick = async () => {
         const res = await settingAxios.update({
             _id: data._id,
             data: {
@@ -44,6 +50,66 @@ const Theme: React.FC<ICarsouelImageProps> = ({ data }) => {
             window.$message.success('保存成功');
         }
     };
+    // 点击新增博客主题
+    const createOnClick = () => {
+        setEditTheme('');
+        setOperation('create');
+        setVisible(true);
+    };
+    // 点击编辑博客主题
+    const editOnClick = (key: string) => {
+        setEditTheme(key);
+        setOperation('edit');
+        setObj(JSON.stringify(themeConfig[key]));
+        setName(key);
+        setVisible(true);
+    };
+    // 点击删除博客主题
+    const deleteOnClick = (key: string) => {
+        Modal.confirm({
+            title: '是否删除该主题配置？',
+            onOk: async () => {
+                const newThemeConfig: { [key: string]: any } = {};
+                // 筛选出还存在的keys
+                const keys = Object.keys(themeConfig).filter(
+                    (k: string) => k !== key
+                );
+                // 遍历这些keys赋值给新的themeConfig
+                keys.forEach((k: string) => {
+                    newThemeConfig[k] = themeConfig[key];
+                });
+                // 更新主题配置信息
+                setThemeConfig(newThemeConfig);
+            },
+        });
+    };
+
+    // 取消添加、编辑主题配置
+    const onCancel = () => {
+        setObj('');
+        setName('');
+        setVisible(false);
+    };
+    // 确认编辑、添加主题
+    const onOk = () => {
+        let isError = false;
+        try {
+            JSON.parse(obj);
+        } catch (e) {
+            isError = true;
+        }
+        // 判断json解析是否有错
+        if (isError) {
+            message.error('json解析错误');
+        } else {
+            if (operation === 'create') {
+                themeConfig[name] = JSON.parse(obj);
+            } else {
+                themeConfig[editTheme] = JSON.parse(obj);
+            }
+            onCancel();
+        }
+    };
 
     return (
         <div className={style['wrap']}>
@@ -52,12 +118,12 @@ const Theme: React.FC<ICarsouelImageProps> = ({ data }) => {
                 <div className={style['btn-wrap']}>
                     <Button
                         className={style['btn']}
-                        onClick={save}
+                        onClick={saveOnClick}
                         type="primary"
                     >
                         保存
                     </Button>
-                    <Button onClick={save} type="primary">
+                    <Button onClick={createOnClick} type="primary">
                         新增
                     </Button>
                 </div>
@@ -65,14 +131,66 @@ const Theme: React.FC<ICarsouelImageProps> = ({ data }) => {
 
             {/* 主题 */}
             <div className={style['theme-wrap']}>
-                <div className={style['theme-box']}>
-                    <h4>dark</h4>
-                    <div className={style['operate-wrap']}>
-                        <span>编辑</span>
-                        <span>删除</span>
+                {Object.keys(themeConfig).map((key: string) => (
+                    <div className={style['theme-box']} key={key}>
+                        <h4>{key}</h4>
+                        <div className={style['operate-wrap']}>
+                            <span
+                                onClick={() => {
+                                    editOnClick(key);
+                                }}
+                            >
+                                编辑
+                            </span>
+                            {key !== 'light' && key !== 'dark' && (
+                                <span
+                                    onClick={() => {
+                                        deleteOnClick(key);
+                                    }}
+                                >
+                                    删除
+                                </span>
+                            )}
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            {/* 弹窗 */}
+            <Modal
+                title={`${operation === 'create' ? '新增' : '编辑'}主题配置`}
+                visible={visible}
+                onOk={onOk}
+                onCancel={onCancel}
+            >
+                <div className={style['themenConfig-modal']}>
+                    <Input
+                        disabled={name === 'light' || name === 'dark'}
+                        className="app-input"
+                        value={name}
+                        onChange={(e) => {
+                            setName(e.target.value);
+                        }}
+                        placeholder="请输入主题名称"
+                    />
+                    <div className={style['obj-wrap']}>
+                        <Input.TextArea
+                            style={{ resize: 'none' }}
+                            value={obj}
+                            placeholder="请添加主题配置信息"
+                            onChange={(e) => {
+                                setObj(e.target.value);
+                                try {
+                                    JSON.parse(obj);
+                                } catch (e) {
+                                    message.error(`json解析错误: ${e}`);
+                                }
+                            }}
+                            rows={4}
+                        />
                     </div>
                 </div>
-            </div>
+            </Modal>
         </div>
     );
 };

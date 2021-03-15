da97485e5a2d31931ad742105a8b760d27bd50d0
hejueting <279975246@qq.com>
2021-03-15 20:56:09 +0800

    路由切换时，自动loading

--- src/components/routers/index.tsx
+++ src/components/routers/index.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import style from './style.module.scss';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, useLocation } from 'react-router-dom';
 import AsyncComponents from '../asyncComponent';
 import routerConfig from '../../utils/routersConfig';
+import { showLoading } from '../../utils/help';
 
-const Routers: React.FC = () => {
+interface IRourerProps {}
+
+const Routers: React.FC<IRourerProps> = () => {
+    // location
+    let location = useLocation();
+    // state
+    const [hisPath, setHisPath] = useState<string>('');
+
+    // 切换路由时，不用自动loading的路由
+    const whiteRouters = ['/user/flow', '/user/active', '/record/edit'];
+
+    useEffect(() => {
+        console.log(location.pathname);
+        if (hisPath !== location.pathname) {
+            if (whiteRouters.indexOf(location.pathname) === -1) {
+                showLoading();
+            }
+            setHisPath(location.pathname);
+        }
+    }, [location]);
+
     return (
         <main className={style.main}>
             <Switch>
